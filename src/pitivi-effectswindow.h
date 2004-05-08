b629face67b224c5f21f8d4681bb4485d773f7e9
Guillaume Casanova <casano_g@epitech.net>
2004-05-08 10:42:57 +0000

    effects window files
    git-svn-id: svn+ssh://svn.gnome.org/svn/pitivi/trunk@41 d3729300-e425-0410-8a4c-d956edccc248

---
+++ src/pitivi-effectswindow.h
@@ -0,0 +1,73 @@
+/* 
+ * PiTiVi
+ * Copyright (C) <2004> Edward G. Hervey <hervey_e@epita.fr>
+ *                      Guillaume Casanova <casano_g@epita.fr>
+ *
+ * This software has been written in EPITECH <http://www.epitech.net>
+ * EPITECH is a computer science school in Paris - FRANCE -
+ * under the direction of Flavien Astraud and Jerome Landrieu.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef PITIVI_EFFECTSWINDOW_H
+#define PITIVI_EFFECTSWINDOW_H
+
+/*
+ * Potentially, include other headers on which this header depends.
+ */
+
+/*
+ * Type macros.
+ */
+
+#define PITIVI_EFFECTSWINDOW_TYPE (pitivi_effectswindow_get_type ())
+#define PITIVI_EFFECTSWINDOW(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), PITIVI_EFFECTSWINDOW_TYPE, PitiviEffectsWindow))
+#define PITIVI_EFFECTSWINDOW_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), PITIVI_EFFECTSWINDOW_TYPE, PitiviEffectsWindowClass))
+#define PITIVI_IS_EFFECTSWINDOW(obj) (G_TYPE_CHECK_TYPE ((obj), PITIVI_EFFECTSWINDOW_TYPE))
+#define PITIVI_IS_EFFECTSWINDOW_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), PITIVI_EFFECTSWINDOW_TYPE))
+#define PITIVI_EFFECTSWINDOW_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), PITIVI_EFFECTSWINDOW_TYPE, PitiviEffectsWindowClass))
+
+typedef struct _PitiviEffectsWindow PitiviEffectsWindow;
+typedef struct _PitiviEffectsWindowClass PitiviEffectsWindowClass;
+typedef struct _PitiviEffectsWindowPrivate PitiviEffectsWindowPrivate;
+
+struct _PitiviEffectsWindow
+{
+  GObject parent;
+
+  /* instance public members */
+
+  /* private */
+  PitiviEffectsWindowPrivate *private;
+};
+
+struct _PitiviEffectsWindowClass
+{
+  GObjectClass parent;
+  /* class members */
+};
+
+/* used by PITIVI_EFFECTSWINDOW_TYPE */
+GType pitivi_effectswindow_get_type (void);
+
+/*
+ * Method definitions.
+ */
+
+PitiviEffectsWindow	*pitivi_effectswindow_new(void);
+
+#endif
