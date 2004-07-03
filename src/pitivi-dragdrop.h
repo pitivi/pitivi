712335f800e251fc5cde28e236e0263e648b3fc1
Guillaume Casanova <casano_g@epitech.net>
2004-07-03 21:16:50 +0000

    oki
    git-svn-id: svn+ssh://svn.gnome.org/svn/pitivi/trunk@192 d3729300-e425-0410-8a4c-d956edccc248

---
+++ src/pitivi-dragdrop.h
@@ -0,0 +1,43 @@
+/* 
+ * PiTiVi
+ * Copyright (C) <2004> Edward G. Hervey <hervey_e@epita.fr>
+ *                      Guillaume Casanova <casano_g@epita.fr>
+ *
+ * This software has been written in EPITECH <http://www.epitech.net>
+ * EPITECH is a computer science school in Paris - FRANCE -
+ * under the direction of Flavien Astraud and Jerome Landrieu.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef PITIVI_DRAGDROP_H
+#define PITIVI_DRAGDROP_H
+
+enum
+  {
+    DND_TARGET_STRING = 1,
+    DND_TARGET_URL = 2
+  };
+
+static GtkTargetEntry TargetEntries[] =
+  {
+    { "text/plain",    0, DND_TARGET_STRING },
+    { "text/uri-list", 0, DND_TARGET_URL },
+  };
+
+static gint iNbTargetEntries = sizeof(TargetEntries)/sizeof(TargetEntries[0]);
+
+#endif /* PITIVI_DRAGDROP_H */
