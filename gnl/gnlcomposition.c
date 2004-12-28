915e0bda8fb2b917b30689c9f56b73cc485cec47
Edward Hervey <edward.hervey@collabora.co.uk>
2004-12-28 15:08:31 +0000

    Composition _prepare() doesn't segfault if empty
    git-svn-id: svn+ssh://svn.gnome.org/svn/pitivi/trunk@537 d3729300-e425-0410-8a4c-d956edccc248

--- gnl/gnlcomposition.c
+++ gnl/gnlcomposition.c
@@ -785,16 +785,17 @@ gnl_composition_prepare (GnlObject *object, GstEvent *event)
   res = gnl_composition_schedule_entries (comp, start_pos,
 					  stop_pos, 1, &pad);
 
-  if (GST_PAD_IS_LINKED(pad)) {
-    GST_WARNING ("pad %s:%s returned by scheduling is connected to %s:%s",
-		 GST_DEBUG_PAD_NAME(pad),
-		 GST_DEBUG_PAD_NAME(GST_PAD_PEER(pad)));
-    gst_pad_unlink (pad, GST_PAD_PEER (pad));
-  }
-
   if (pad) {
 
     GST_INFO("Have a pad");
+
+    if (GST_PAD_IS_LINKED(pad)) {
+      GST_WARNING ("pad %s:%s returned by scheduling is connected to %s:%s",
+		   GST_DEBUG_PAD_NAME(pad),
+		   GST_DEBUG_PAD_NAME(GST_PAD_PEER(pad)));
+      gst_pad_unlink (pad, GST_PAD_PEER (pad));
+    }
+    
 
     GST_INFO ("Putting probe and ghost pad back");
     probe = gst_probe_new (FALSE, probe_fired, comp);
