be2b0814116e162f3d5a557179a5ef54e2ee40a9
Edward Hervey <edward.hervey@collabora.co.uk>
2006-03-13 10:52:20 +0000

    * autogen.sh: Remove path for non-existing directory for aclocal inclusions.
    git-svn-id: svn+ssh://svn.gnome.org/svn/pitivi/trunk@706 d3729300-e425-0410-8a4c-d956edccc248

--- autogen.sh
+++ autogen.sh
@@ -72,7 +72,7 @@ toplevel_check $srcfile
 #tool_run "$autopoint --force"
 #patch -p0 < common/gettext.patch
 
-tool_run "$aclocal" "-I m4 -I common/m4 $ACLOCAL_FLAGS"
+tool_run "$aclocal" "-I common/m4 $ACLOCAL_FLAGS"
 tool_run "$libtoolize" "--copy --force"
 # tool_run "$autoheader"
 
