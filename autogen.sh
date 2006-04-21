055a505d3ce961988e93db29a9be6c85111faaa2
Andy Wingo <wingo@pobox.com>
2006-04-21 15:18:31 +0000

    2006-04-21  Andy Wingo  <wingo@pobox.com>
    	* configure.ac: Remove calls to libtool.
    
    	* autogen.sh: Update to simplify, not run libtoolize.
    
    
    git-svn-id: svn+ssh://svn.gnome.org/svn/pitivi/trunk@758 d3729300-e425-0410-8a4c-d956edccc248

--- autogen.sh
+++ autogen.sh
@@ -35,10 +35,6 @@ version_check "autoconf" "$AUTOCONF autoconf autoconf-2.54 autoconf-2.53 autocon
               "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || DIE=1
 version_check "automake" "$AUTOMAKE automake automake-1.7 automake-1.6 automake-1.5" \
               "ftp://ftp.gnu.org/pub/gnu/automake/" 1 6 || DIE=1
-version_check "autopoint" "autopoint" \
-              "ftp://ftp.gnu.org/pub/gnu/gettext/" 0 11 5 || DIE=1
-version_check "libtoolize" "$LIBTOOLIZE libtoolize" \
-              "ftp://ftp.gnu.org/pub/gnu/libtool/" 1 5 0 || DIE=1
 version_check "pkg-config" "" \
               "http://www.freedesktop.org/software/pkgconfig" 0 8 0 || DIE=1
 
@@ -61,40 +57,9 @@ fi
 
 toplevel_check $srcfile
 
-# autopoint
-#    older autopoint (< 0.12) has a tendency to complain about mkinstalldirs
-#if test -x mkinstalldirs; then rm mkinstalldirs; fi
-#    first remove patch if necessary, then run autopoint, then reapply
-#if test -f po/Makefile.in.in;
-#then
-#  patch -p0 -R < common/gettext.patch
-#fi
-#tool_run "$autopoint --force"
-#patch -p0 < common/gettext.patch
-
 tool_run "$aclocal" "-I common/m4 $ACLOCAL_FLAGS"
-tool_run "$libtoolize" "--copy --force"
-# tool_run "$autoheader"
-
-# touch the stamp-h.in build stamp so we don't re-run autoheader in maintainer mode -- wingo
-echo timestamp > stamp-h.in 2> /dev/null
-
 tool_run "$autoconf"
 tool_run "$automake" "-a -c"
-
-# if enable exists, add an -enable option for each of the lines in that file
-if test -f enable; then
-  for a in `cat enable`; do
-    CONFIGURE_FILE_OPT="--enable-$a"
-  done
-fi
-
-# if disable exists, add an -disable option for each of the lines in that file
-if test -f disable; then
-  for a in `cat disable`; do
-    CONFIGURE_FILE_OPT="$CONFIGURE_FILE_OPT --disable-$a"
-  done
-fi
 
 test -n "$NOCONFIGURE" && {
   echo "+ skipping configure stage for package $package, as requested."
@@ -105,19 +70,9 @@ test -n "$NOCONFIGURE" && {
 echo "+ running configure ... "
 test ! -z "$CONFIGURE_DEF_OPT" && echo "  ./configure default flags: $CONFIGURE_DEF_OPT"
 test ! -z "$CONFIGURE_EXT_OPT" && echo "  ./configure external flags: $CONFIGURE_EXT_OPT"
-test ! -z "$CONFIGURE_FILE_OPT" && echo "  ./configure enable/disable flags: $CONFIGURE_FILE_OPT"
 echo
 
-./configure $CONFIGURE_DEF_OPT $CONFIGURE_EXT_OPT $CONFIGURE_FILE_OPT || {
+./configure $CONFIGURE_DEF_OPT $CONFIGURE_EXT_OPT || {
         echo "  configure failed"
         exit 1
 }
-
-# aclocal -I common || exit 1
-# libtoolize --force || exit 1
-# # autoheader || exit 1
-# autoconf || exit 1
-# automake -a || exit 1
-# echo "./autogen.sh $@" > autoregen.sh
-# chmod +x autoregen.sh
-# ./configure $@
