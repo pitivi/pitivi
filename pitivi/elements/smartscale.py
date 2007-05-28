6524c6c6b9fbe513dd474fd8b5ced8080050d338
Edward Hervey <edward.hervey@collabora.co.uk>
2007-05-28 10:15:01 +0000

    * pitivi/elements/smartscale.py: Always set the pixel-aspect-ratio on the intermediate capsfilter to handle video streams that don't specify the PAR (which in that case defaults to 1/1).
    git-svn-id: svn+ssh://svn.gnome.org/svn/pitivi/trunk@958 d3729300-e425-0410-8a4c-d956edccc248

--- pitivi/elements/smartscale.py
+++ pitivi/elements/smartscale.py
@@ -145,7 +145,9 @@ class SmartVideoScale(gst.Bin):
         if self.darin == self.darout:
             self.log("We have same input and output caps, resetting capsfilter and videobox settings")
             # same DAR, set inputcaps on capsfilter, reset videobox values
-            caps = gst.caps_new_any()
+            astr = "width=%d,height=%d,pixel-aspect-ratio=%d/%d" % (self.widthout, self.heightout,
+                                                                    self.parout.num,
+                                                                    self.parout.denom)
             left = 0
             right = 0
             top = 0
@@ -164,7 +166,9 @@ class SmartVideoScale(gst.Bin):
                 bottom = extra - top # compensate for odd extra
                 left = right = 0
                 # calculate filter caps
-                astr = "width=%d,height=%d" % (self.widthout, newheight)
+                astr = "width=%d,height=%d,pixel-aspect-ratio=%d/%d" % (self.widthout, newheight,
+                                                                        self.parout.num,
+                                                                        self.parout.denom)
             else:
                 self.log("incoming DAR is smaller than outgoing DAR. Adding left/right borders")
                 # height, PAR stays the same as output
@@ -176,8 +180,10 @@ class SmartVideoScale(gst.Bin):
                 right = extra - left # compensate for odd extra
                 top = bottom = 0
                 # calculate filter caps
-                astr = "width=%d,height=%d" % (newwidth, self.heightout)
-            caps = gst.caps_from_string("video/x-raw-yuv,%s;video/x-raw-rgb,%s" % (astr, astr))
+                astr = "width=%d,height=%d,pixel-aspect-ratio=%d/%d" % (newwidth, self.heightout,
+                                                                        self.parout.num,
+                                                                        self.parout.denom)
+        caps = gst.caps_from_string("video/x-raw-yuv,%s;video/x-raw-rgb,%s" % (astr, astr))
 
         # set properties on elements
         self.debug("About to set left/right/top/bottom : %d/%d/%d/%d" % (-left, -right, -top, -bottom))
