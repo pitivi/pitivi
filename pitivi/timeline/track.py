61b261abecc3eee5b0c81182073ee25606ee4772
Alessandro Decina <alessandro.d@gmail.com>
2009-01-05 03:21:00 +0100

    Implement TrackObject. With unit tests.

---
+++ pitivi/timeline/track.py
@@ -0,0 +1,133 @@
+# PiTiVi , Non-linear video editor
+#
+#       pitivi/timeline/timeline.py
+#
+# Copyright (c) 2009, Alessandro Decina <alessandro.decina@collabora.co.uk>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+import gst
+
+from pitivi.signalinterface import Signallable
+from pitivi.utils import UNKNOWN_DURATION
+
+class TrackError(Exception):
+    pass
+
+class TrackObject(Signallable):
+    __signals__ = {
+        'start-changed': ['start'],
+        'duration-changed': ['duration'],
+        'in-point-changed': ['in-point'],
+        'out-point-changed': ['out-point']
+    }
+
+    def __init__(self, factory, start=0, 
+            duration=UNKNOWN_DURATION, in_point=gst.CLOCK_TIME_NONE,
+            out_point=UNKNOWN_DURATION, priority=0):
+        self.factory = factory
+        self.track_objects = []
+        self.gnl_object = obj = self._makeGnlObject()
+        obj.props.start = start
+        obj.props.duration = duration
+        obj.props.media_start = in_point
+        obj.props.media_duration = out_point
+        self._connectToSignals(obj)
+
+    # FIXME: there's a lot of boilerplate here that could be factored in a metaclass.
+    # Do we like metaclasses in pitivi?
+    def _getStart(self):
+        return self.gnl_object.props.start
+    
+    def _setStart(self, value):
+        self.gnl_object.props.start = value
+
+    start = property(_getStart, _setStart)
+
+    def _getDuration(self):
+        return self.gnl_object.props.duration
+    
+    def _setDuration(self, value):
+        self.gnl_object.props.duration = value
+    
+    duration = property(_getDuration, _setDuration)
+
+    def _getInPoint(self):
+        return self.gnl_object.props.media_start
+    
+    def _setInPoint(self, value):
+        self.gnl_object.props.media_start = value
+    
+    in_point = property(_getInPoint, _setInPoint)
+
+    def _getOutPoint(self):
+        return self.gnl_object.props.media_duration
+    
+    def _setOutPoint(self, value):
+        self.gnl_object.props.media_duration = value
+
+    out_point = property(_getOutPoint, _setOutPoint)
+
+    def _notifyStartCb(self, obj, pspec):
+        self.emit('start-changed', obj.props.start)
+    
+    def _notifyDurationCb(self, obj, pspec):
+        self.emit('duration-changed', obj.props.duration)
+    
+    def _notifyMediaStartCb(self, obj, pspec):
+        self.emit('in-point-changed', obj.props.media_start)
+    
+    def _notifyMediaDurationCb(self, obj, pspec):
+        self.emit('out-point-changed', obj.props.media_duration)
+
+    def _connectToSignals(self, gnl_object):
+        gnl_object.connect('notify::start', self._notifyStartCb)
+        gnl_object.connect('notify::duration', self._notifyDurationCb)
+        gnl_object.connect('notify::media-start', self._notifyMediaStartCb)
+        gnl_object.connect('notify::media-duration',
+                self._notifyMediaDurationCb)
+
+    def _makeGnlObject(self):
+        raise NotImplementedError()
+
+
+class SourceTrackObject(TrackObject):
+    def _makeGnlObject(self):
+        source = gst.element_factory_make('gnlsource')
+        return source
+
+# FIXME: effects?
+
+class Track(Signallable):
+    def __init__(self):
+        self.composition = gst.element_factory_make('gnlcomposition')
+        self.objects = set([])
+
+    def addObject(self, track_object):
+        try:
+            self.composition.add(track_object.gnl_object)
+        except gst.AddError:
+            raise TrackError()
+
+        self.track_objects.append(track_object)
+
+    def removeObject(self, track_object):
+        try:
+            self.composition.remove(track_object.gnl_object)
+        except gst.RemoveError:
+            raise TrackError()
+
+        self.track_objects.remove(track_object)
---
+++ tests/test_track.py
@@ -0,0 +1,125 @@
+# PiTiVi , Non-linear video editor
+#
+#       pitivi/timeline/timeline.py
+#
+# Copyright (c) 2009, Alessandro Decina <alessandro.decina@collabora.co.uk>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+from unittest import TestCase
+import gst
+
+from pitivi.timeline.track import Track, SourceTrackObject, TrackError
+from pitivi.stream import AudioStream, VideoStream
+from pitivi.utils import UNKNOWN_DURATION
+
+class StubFactory(object):
+    pass
+
+class TrackObjectSignalMonitor(object):
+    def __init__(self, track_object):
+        self.track_object = track_object
+        
+        track_object.connect('start-changed', self._signal_cb, 'start_changed')
+        track_object.connect('duration-changed', self._signal_cb, 'duration_changed')
+        track_object.connect('in-point-changed', self._signal_cb, 'in_point_changed')
+        track_object.connect('out-point-changed', self._signal_cb, 'out_point_changed')
+
+        self.start_changed_count = 0
+        self.duration_changed_count = 0
+        self.in_point_changed_count = 0
+        self.out_point_changed_count = 0
+
+    def _signal_cb(self, obj, value, name):
+        field = '%s_count' % name
+        setattr(self, field, getattr(self, field, 0) + 1)
+
+class TestTrackObject(TestCase):
+    def setUp(self):
+        factory = StubFactory()
+        self.track_object = SourceTrackObject(factory)
+        self.monitor = TrackObjectSignalMonitor(self.track_object)
+
+    def testDefaultProperties(self):
+        obj = self.track_object
+        self.failUnlessEqual(obj.start, 0)
+        self.failUnlessEqual(obj.duration, UNKNOWN_DURATION)
+        self.failUnlessEqual(obj.in_point, gst.CLOCK_TIME_NONE)
+        self.failUnlessEqual(obj.out_point, UNKNOWN_DURATION)
+
+        gnl_object = obj.gnl_object
+        self.failUnlessEqual(gnl_object.props.start, 0)
+        self.failUnlessEqual(gnl_object.props.duration, UNKNOWN_DURATION)
+        self.failUnlessEqual(gnl_object.props.media_start,
+                gst.CLOCK_TIME_NONE)
+        self.failUnlessEqual(gnl_object.props.media_duration,
+                UNKNOWN_DURATION)
+
+    def testChangePropertiesFromTrackObject(self):
+        obj = self.track_object
+        gnl_object = obj.gnl_object
+       
+        start = 1 * gst.SECOND
+        obj.start = start
+        self.failUnlessEqual(obj.start, start)
+        self.failUnlessEqual(gnl_object.props.start, start)
+        self.failUnlessEqual(self.monitor.start_changed_count, 1)
+
+        duration = 10 * gst.SECOND
+        obj.duration = duration
+        self.failUnlessEqual(obj.duration, duration)
+        self.failUnlessEqual(gnl_object.props.duration, duration)
+        self.failUnlessEqual(self.monitor.duration_changed_count, 1)
+
+        in_point = 5 * gst.SECOND
+        obj.in_point = in_point
+        self.failUnlessEqual(obj.in_point, in_point)
+        self.failUnlessEqual(gnl_object.props.media_start, in_point)
+        self.failUnlessEqual(self.monitor.in_point_changed_count, 1)
+        
+        out_point = 5 * gst.SECOND
+        obj.out_point = out_point
+        self.failUnlessEqual(obj.out_point, out_point)
+        self.failUnlessEqual(gnl_object.props.media_duration, out_point)
+        self.failUnlessEqual(self.monitor.out_point_changed_count, 1)
+
+    def testChangePropertiesFromGnlObject(self):
+        obj = self.track_object
+        gnl_object = obj.gnl_object
+       
+        start = 1 * gst.SECOND
+        gnl_object.props.start = start
+        self.failUnlessEqual(obj.start, start)
+        self.failUnlessEqual(self.monitor.start_changed_count, 1)
+
+        duration = 10 * gst.SECOND
+        gnl_object.props.duration = duration
+        self.failUnlessEqual(obj.duration, duration)
+        self.failUnlessEqual(self.monitor.duration_changed_count, 1)
+
+        in_point = 5 * gst.SECOND
+        gnl_object.props.media_start = in_point
+        self.failUnlessEqual(obj.in_point, in_point)
+        self.failUnlessEqual(self.monitor.in_point_changed_count, 1)
+        
+        out_point = 5 * gst.SECOND
+        gnl_object.props.media_duration = out_point
+        self.failUnlessEqual(obj.out_point, out_point)
+        self.failUnlessEqual(self.monitor.out_point_changed_count, 1)
+
+class TestTrackAddRemoveObjects(TestCase):
+    def testAddRemoveObjects(self):
+        pass
