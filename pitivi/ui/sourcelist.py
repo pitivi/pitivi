pitivi/pitivi

commit 6f623e01b7dce7df52b38b6c855f19059306f669

sourcelist: only call drag_begin at appropriate time, Finally Fixes 572327

pitivi/ui/sourcelist.py
@@ -541,6 +541,16 @@ class SourceList(gtk.VBox, Loggable):
     _dragY = 0
     _ignoreRelease = False
 
+    def _rowUnderMouseSelected(self, treeview, event):
+        result = treeview.get_path_at_pos(int(event.x), int(event.y))
+        if result:
+            path = result[0]
+            return treeview.get_selection().path_is_selected(path)
+        return False
+
+    def _nothingUnderMouse(self, treeview, event):
+        return not bool(treeview.get_path_at_pos(int(event.x), int(event.y)))
+
     def _treeViewButtonPressEventCb(self, treeview, event):
         chain_up = True
 
@@ -551,7 +561,7 @@ class SourceList(gtk.VBox, Loggable):
         else:
 
             if not event.state & (gtk.gdk.CONTROL_MASK | gtk.gdk.SHIFT_MASK):
-                chain_up = False
+                chain_up = not self._rowUnderMouseSelected(treeview, event)
 
             self._dragStarted = False
             self._dragButton = event.button 
@@ -568,16 +578,20 @@ class SourceList(gtk.VBox, Loggable):
         return True
 
     def _treeViewMotionNotifyEventCb(self, treeview, event):
-        if self._dragButton:
-            if treeview.drag_check_threshold(self._dragX, self._dragY,
-                int(event.x), int(event.y)):
-                context = treeview.drag_begin(
-                    [dnd.URI_TUPLE, dnd.FILESOURCE_TUPLE],
-                    gtk.gdk.ACTION_COPY,
-                    self._dragButton,
-                    event)
-                self._dragStarted = True
+        if not self._dragButton:
             return True
+
+        if self._nothingUnderMouse(treeview, event):
+            return True
+
+        if treeview.drag_check_threshold(self._dragX, self._dragY,
+            int(event.x), int(event.y)):
+            context = treeview.drag_begin(
+                [dnd.URI_TUPLE, dnd.FILESOURCE_TUPLE],
+                gtk.gdk.ACTION_COPY,
+                self._dragButton,
+                event)
+            self._dragStarted = True
         return False
 
     def _treeViewButtonReleaseCb(self, treeview, event):

commit bbd1683a7c148e202df50c7dd03b3ad0db0b0fa5

Fix double clicking in the source list. Fixes #589807.
Double clicking was broken in 6f623e01, which fixed #572327. Hopefully I didn't
break it again.

pitivi/ui/sourcelist.py
@@ -585,7 +585,10 @@ class SourceList(gtk.VBox, Loggable):
         result = treeview.get_path_at_pos(int(event.x), int(event.y))
         if result:
             path = result[0]
-            return treeview.get_selection().path_is_selected(path)
+            selection = treeview.get_selection()
+
+            return selection.path_is_selected(path) and selection.count_selected_rows() > 1
+
         return False
 
     def _nothingUnderMouse(self, treeview, event):