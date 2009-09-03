e574b90f0e5bf1f167652c08ed43fed8d9e691b4
Edward Hervey <bilboed@bilboed.com>
2009-09-03 19:44:09 +0200

    SingleDecodeBin: Set the uri source block size to 512kbytes.
    This will only affect sources used in push-based mode, like when used in combination
    with jpegdec. This speeds up considerably decoding/usage of jpeg images.

--- pitivi/elements/singledecodebin.py
+++ pitivi/elements/singledecodebin.py
@@ -71,6 +71,9 @@ class SingleDecodeBin(gst.Bin):
             self.log("created urisrc %s / %r" % (self.urisrc.get_name(),
                                                  self.urisrc))
             self.add(self.urisrc)
+            # Set the blocksize to 512kbytes, this will only matter for push-based sources
+            if hasattr(self.urisrc.props, "blocksize"):
+                self.urisrc.props.blocksize = 524288
             self.urisrc.link(self.typefind)
         else:
             self._sinkpad = gst.GhostPad("sink", self.typefind.get_pad("sink"))
