1ac892da095598d272f292bb240e3e69a72b1043
Brandon Lewis <brandon_lewis@alum.berkeley.edu>
2010-01-20 17:53:46 -0800

    pitivi/factories/base.py: clear ghost pad target before removing it

--- pitivi/factories/base.py
+++ pitivi/factories/base.py
@@ -440,8 +440,14 @@ class SourceFactory(ObjectFactory):
 
     def _singlePadRemovedCb(self, dbin, pad, topbin):
         self.debug("dbin:%r, pad:%r, topbin:%r", dbin, pad, topbin)
+
+        # work around for http://bugzilla.gnome.org/show_bug.cgi?id=590735
+        die = gst.Pad("die", gst.PAD_SRC)
+        topbin.ghostpad.set_target(die)
+
         topbin.remove_pad(topbin.ghostpad)
         del topbin.ghostpad
+
         if hasattr(topbin, "volume"):
             pad.unlink(topbin.aconv.get_pad("sink"))
         elif hasattr(topbin, "alpha"):
