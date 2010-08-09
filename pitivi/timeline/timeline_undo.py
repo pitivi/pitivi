bed6ae91cc7640ba3589ecddcdd1185323910f7e
Alessandro Decina <alessandro.d@gmail.com>
2010-08-09 16:54:41 +0200

    timeline/timeline_undo: update method to reflect a change in the keyframe-removed signature.

--- pitivi/timeline/timeline_undo.py
+++ pitivi/timeline/timeline_undo.py
@@ -303,7 +303,8 @@ class TimelineLogObserver(object):
         action = self.interpolatorKeyframeAddedAction(track_object, keyframe)
         self.log.push(action)
 
-    def _interpolatorKeyframeRemovedCb(self, track_object, keyframe):
+    def _interpolatorKeyframeRemovedCb(self, track_object, keyframe,
+            old_value=None):
         action = self.interpolatorKeyframeRemovedAction(track_object, keyframe)
         self.log.push(action)
 
