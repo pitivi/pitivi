acd62daf909bacb956090e215547a84208507e41
Alessandro Decina <alessandro.decina@collabora.co.uk>
2010-08-10 16:04:41 +0200

    tests: run test_integration only if TEST_INTEGRATION is set.
    Allows me to use trial and nose to run the tests without test_integration
    failing.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1077,5 +1077,17 @@ class TestTransitions(Base):
     def testSaveAndLoadWithTransitions(self):
         pass
 
+# Readable code ends here <-
+
+if not os.getenv("TEST_INTEGRATION"):
+    import inspect
+
+    for symbol_name in list(globals().keys()):
+        print symbol_name
+        symbol = globals()[symbol_name]
+        if inspect.isclass(symbol) and symbol.__name__.startswith('Test') \
+                and symbol.__module__ == __name__:
+            del globals()[symbol_name]
+
 if __name__ == "__main__":
     unittest.main()
