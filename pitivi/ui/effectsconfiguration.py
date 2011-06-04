c34a5a77f43ef1f2ee647b9dd415553ba2b26da5
Hicham HAOUARI <hicham.haouari@gmail.com>
2011-06-05 00:08:38 +0100

    Fix typo in license

--- pitivi/ui/effectsconfiguration.py
+++ pitivi/ui/effectsconfiguration.py
@@ -17,10 +17,6 @@
 # License along with this program; if not, write to the
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
 
 import gst
 import gtk
