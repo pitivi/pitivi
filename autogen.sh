23bed12e31592b6a980459fc4555597587b47730
Thibault Saunier <thibault.saunier@collabora.com>
2011-06-22 23:42:23 -0400

    Add a pre-hook commit to check the pep8 compliance of each commit

--- autogen.sh
+++ autogen.sh
@@ -42,6 +42,13 @@ autoheader_check || DIE=1
 
 die_check $DIE
 
+# install pre-commit hook for doing clean commits
+if test ! \( -x .git/hooks/pre-commit -a -L .git/hooks/pre-commit \);
+then
+    rm -f .git/hooks/pre-commit
+    ln -s ../../pre-commit.hook .git/hooks/pre-commit
+fi
+
 # if no arguments specified then this will be printed
 if test -z "$*"; then
   echo "+ checking for autogen.sh options"
