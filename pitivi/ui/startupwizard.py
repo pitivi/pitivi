18fa26b4c991478ea14a15a01c8f81ec623ff75c
Alex Băluț <alexandru.balut@gmail.com>
2011-07-10 10:44:36 +0200

    Add copyright notice to startupwizard.py

--- pitivi/ui/startupwizard.py
+++ pitivi/ui/startupwizard.py
@@ -1,4 +1,23 @@
-"""Dialog box to quickstart Pitivi"""
+# PiTiVi , Non-linear video editor
+#
+#       pitivi/ui/startupwizard.py
+#
+# Copyright (c) 2010 Mathieu Duponchelle <seeed@laposte.net>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+# Boston, MA 02110-1301, USA.
 
 import os
 import gtk
@@ -11,12 +30,14 @@ from urllib import unquote
 
 
 class StartUpWizard(object):
-    """A Wizard displaying recent projects and allowing the user to either:
+    """A Wizard displaying recent projects.
 
-    load one, skip,see the quick start manual or
-
-    configure a new project with the settings dialog.
-
+    Allows the user to:
+    - create a new project and open the settings dialog (Create button),
+    - create a new project with the default settings (dialog close or ESC),
+    - load a recently opened project (double click recent chooser),
+    - load a project (Browse button),
+    - see the quick start manual (User Manual button).
     """
 
     def __init__(self, app):
