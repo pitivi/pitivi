8e2b19af25376a673c1d06bf76d6e34712ca9a2f
Jean-François Fortin Tam <nekohayo@gmail.com>
2012-01-01 20:11:43 -0500

    tests/test_system.py: test that adding and removing keys occurs properly tests/test_system_gnome.py: test powersaving works on GNOME

---
+++ tests/test_system.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+from pitivi.system import System
+
+class TestSystem(TestCase):
+    def setUp(self):
+        self.system = System()
+
+    def testScreensaverInhibit(self):
+        #check that we start of uninhibited
+        self.assertTrue(not self.system.screensaverIsInhibited())
+
+        #inhibit and check that we are
+        self.system.inhibitScreensaver("a")
+        self.assertTrue(self.system.screensaverIsInhibited())
+        #uninhibit and check that we are
+        self.system.uninhibitScreensaver("a")
+        self.assertTrue(not self.system.screensaverIsInhibited())
+
+        #check that adding/removing is consistent with multiple keys
+        for c in range(0, 5):
+            self.system.inhibitScreensaver(str(c))
+            self.assertTrue(self.system.screensaverIsInhibited(str(c)))
+
+        for c in range (0, 5):
+            self.system.uninhibitScreensaver(str(c))
+            self.assertTrue(not self.system.screensaverIsInhibited(str(c))) 
+
+        self.assertTrue(not self.system.screensaverIsInhibited())
+
+    def testSleepInhibit(self):
+        #check that we start of uninhibited
+        self.assertTrue(not self.system.sleepIsInhibited())
+
+        #inhibit and check that we are
+        self.system.inhibitSleep("a")
+        self.assertTrue(self.system.sleepIsInhibited())
+        #uninhibit and check that we are
+        self.system.uninhibitSleep("a")
+        self.assertTrue(not self.system.sleepIsInhibited())
+
+        #check that adding/removing is consistent with multiple keys
+        for c in range(0, 5):
+            self.system.inhibitSleep(str(c))
+            self.assertTrue(self.system.sleepIsInhibited(str(c)))
+
+        for c in range (0, 5):
+            self.system.uninhibitSleep(str(c))
+            self.assertTrue(not self.system.sleepIsInhibited(str(c)))
+
+        self.assertTrue(not self.system.sleepIsInhibited())
