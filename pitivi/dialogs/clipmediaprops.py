c892d011ef4df6b1f6bbad91632673aae3ec0955
Mathieu Duponchelle <mduponchelle1@gmail.com>
2013-12-26 15:33:31 +0100

    clipmediaprops: Switch to new-class style.

--- pitivi/dialogs/clipmediaprops.py
+++ pitivi/dialogs/clipmediaprops.py
@@ -34,7 +34,7 @@ from pitivi.utils.ui import frame_rates, audio_rates, \
     audio_channels, pixel_aspect_ratios, get_value_from_model
 
 
-class ClipMediaPropsDialog():
+class ClipMediaPropsDialog(object):
     """
     Displays an asset's properties, and allow applying them to a project.
     """
