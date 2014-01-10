10878048801651ab444f8dfaa2fbe7354f94739d
Alexandru Băluț <alexandru.balut@gmail.com>
2014-01-10 20:21:29 +0100

    ui: Remove unused function to draw rounded rectangles in Cairo

--- pitivi/utils/ui.py
+++ pitivi/utils/ui.py
@@ -342,30 +342,6 @@ def beautify_ETA(length):
     return ", ".join(parts)
 
 
-#--------------------- UI drawing helper -------------------------------------#
-# from http://cairographics.org/cookbook/roundedrectangles/
-def roundedrec(context, x, y, w, h, r=10):
-    "Draw a rounded rectangle"
-    #   A****BQ
-    #  H      C
-    #  *      *
-    #  G      D
-    #   F****E
-
-    context.move_to(x + r, y)      # Move to A
-    context.line_to(x + w - r, y)  # Straight line to B
-
-    # Curve to C, Control points are both at Q
-    context.curve_to(x + w, y, x + w, y, x + w, y + r)
-    context.line_to(x + w, y + h - r)                               # Move to D
-    context.curve_to(x + w, y + h, x + w, y + h, x + w - r, y + h)  # Curve to E
-    context.line_to(x + r, y + h)                                   # Line to F
-    context.curve_to(x, y + h, x, y + h, x, y + h - r)              # Curve to G
-    context.line_to(x, y + r)                                       # Line to H
-    context.curve_to(x, y, x, y, x + r, y)                          # Curve to A
-    return
-
-
 #--------------------- Gtk widget helpers ------------------------------------#
 def model(columns, data):
     ret = Gtk.ListStore(*columns)
