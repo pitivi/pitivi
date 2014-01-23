f25f4ad4505867647328b3fe8a2a4bff117edb58
Daniel Mustieles <daniel.mustieles@gmail.com>
2014-01-23 12:48:21 +0100

    Updated FSF's address

--- pitivi/timeline/__init__.py
+++ pitivi/timeline/__init__.py
@@ -15,9 +15,7 @@
 # Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
+# License along with this program; if not, see <http://www.gnu.org/licenses/>.
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
