8e9a62931adbff9912e9a318b9576455be118ecf
Alexandru Băluț <alexandru.balut@gmail.com>
2014-03-05 03:52:48 +0100

    dogtail: Remove testing of missing font chooser element

--- tests/dogtail_scripts/test_dialogs_prefs.py
+++ tests/dogtail_scripts/test_dialogs_prefs.py
@@ -13,19 +13,6 @@ class DialogsPreferencesTest(HelpFunc):
         dialog = self.pitivi.child(name="Preferences", roleName="dialog", recursive=False)
         dialog.child("Reset to Factory Settings", roleName="push button").click()
 
-        # Set a different font
-        dialog.child(name="Sans", roleName="push button").click()
-        fontchooser = self.pitivi.child(name="Pick a Font", roleName="dialog", recursive=False)
-        # Perf hack: do a search to reduce the amount of table cells displayed
-        # (otherwise dogtail will take ages to search for it) and to prevent
-        # scrolling (so we can just click the first item in the search results).
-        # Since the search entry is focused by default, just start typing:
-        dogtail.rawinput.typeText("cantarell bold oblique")
-        # Ideally I'd search the child table cells, but this is way too slow,
-        # so let's just pick the first item in the search results:
-        fontchooser.child(roleName="table cell").click()
-        fontchooser.child(name="Select", roleName="push button").click()
-
         # Set the thumbnail gap setting (or whatever the first spinbutton is)
         foo = dialog.child(roleName="spin button")
         # The following is quite silly. You *need* to focus the widget
@@ -45,11 +32,6 @@ class DialogsPreferencesTest(HelpFunc):
         dialog = self.pitivi.child(name="Preferences", roleName="dialog", recursive=False)
 
         # Check if the previous values were correctly saved
-        # In the case of the font, just search if such an item exists:
-        try:
-            dialog.child(name="Cantarell Bold Oblique", roleName="push button")
-        except SearchError:
-            self.fail("Font was not saved")
         self.assertEqual(dialog.child(roleName="spin button").text, "12")
 
         # Check the "revert to last user values" feature
@@ -64,5 +46,4 @@ class DialogsPreferencesTest(HelpFunc):
 
         # Check resetting to factory settings
         dialog.child("Reset to Factory Settings", roleName="push button").click()
-        dialog.child(name="Sans", roleName="push button")
         self.assertEqual(dialog.child(roleName="spin button").text, "5", "Resetting to factory defaults failed")
