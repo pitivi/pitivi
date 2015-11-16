d8c575df30c660f4a13f879c671ede9cda1abd1e
Jean-François Fortin Tam <nekohayo@gmail.com>
2015-11-16 15:55:43 -0500

    mainwindow: Remove dead size settings
    Advanced element/codec settings dialogs have auto-sized for a while now

--- pitivi/mainwindow.py
+++ pitivi/mainwindow.py
@@ -89,14 +89,6 @@ GlobalSettings.addConfigOption('lastExportFolder',
                                key="last-export-folder",
                                environment="PITIVI_EXPORT_FOLDER",
                                default=os.path.expanduser("~"))
-GlobalSettings.addConfigOption('elementSettingsDialogWidth',
-                               section='export',
-                               key='element-settings-dialog-width',
-                               default=620)
-GlobalSettings.addConfigOption('elementSettingsDialogHeight',
-                               section='export',
-                               key='element-settings-dialog-height',
-                               default=460)
 GlobalSettings.addConfigSection("effect-configuration")
 GlobalSettings.addConfigOption('effectVPanedPosition',
                                section='effect-configuration',
