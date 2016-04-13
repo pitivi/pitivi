a12d917bad2ee2e49a30ecb2c57bcad562488b38
Alexandru Băluț <alexandru.balut@gmail.com>
2016-04-13 03:56:46 +0200

    Fix autogen.sh
    Get rid of the common submodule as we only need AS_AC_EXPAND from there
    and instead copy it to our m4 dir.
    
    The call to autoreconf replaces the calls to libtoolize, aclocal,
    autoconf and automake, as per "man autoreconf".
    
    Fixes https://phabricator.freedesktop.org/T7382
    
    Reviewed-by: Thibault Saunier <tsaunier@gnome.org>
    Differential Revision: https://phabricator.freedesktop.org/D906

--- autogen.sh
+++ autogen.sh
@@ -2,49 +2,10 @@
 
 export PYTHON=python3
 
-DIE=0
 package=pitivi
 srcfile=pitivi/application.py
 
-if test ! -f common/Makefile.am;
-then
-  rm -R common/
-  echo "+ Setting up common submodule"
-  git submodule init
-fi
-git submodule update
-
-# source helper functions
-if test ! -f common/gst-autogen.sh;
-then
-  echo There is something wrong with your source tree.
-  echo You are missing common/gst-autogen.sh
-  exit 1
-fi
-. common/gst-autogen.sh
-
 CONFIGURE_DEF_OPT=''
-
-autogen_options $@
-
-echo -n "+ check for build tools"
-if test ! -z "$NOCHECK"; then echo " skipped"; else  echo; fi
-version_check "autoconf" "$AUTOCONF autoconf autoconf-2.54 autoconf-2.53 autoconf-2.52" \
-              "ftp://ftp.gnu.org/pub/gnu/autoconf/" 2 52 || DIE=1
-version_check "automake" "$AUTOMAKE automake automake-1.7 automake-1.6 automake-1.5" \
-              "ftp://ftp.gnu.org/pub/gnu/automake/" 1 6 || DIE=1
-version_check "pkg-config" "" \
-              "http://www.freedesktop.org/software/pkgconfig" 0 8 0 || DIE=1
-version_check "libtoolize" "$LIBTOOLIZE libtoolize glibtoolize" \
-              "ftp://ftp.gnu.org/pub/gnu/libtool/" 2 2 6 || DIE=1
-
-die_check $DIE
-
-autoconf_2_52d_check || DIE=1
-aclocal_check || DIE=1
-autoheader_check || DIE=1
-
-die_check $DIE
 
 # if no arguments specified then this will be printed
 if test -z "$*"; then
@@ -54,8 +15,6 @@ if test -z "$*"; then
   echo "  To pass any additional options, please specify them on the $0"
   echo "  command line."
 fi
-
-toplevel_check $srcfile
 
 build_help=true
 while getopts disable-help x; do
@@ -70,27 +29,20 @@ if $build_help; then
   fi
 fi
 
-# This is needed to create ltmain.sh for our C bits.
-tool_run "$libtoolize" "--copy --force"
-tool_run "$aclocal" "-I common/m4 -I m4 $ACLOCAL_FLAGS"
-tool_run "$autoconf"
-tool_run "$automake" "-a -c"
+autoreconf --force --install || exit 1
 
-test -n "$NOCONFIGURE" && {
+if test -n "$NOCONFIGURE"; then
   echo "+ skipping configure stage for package $package, as requested."
   echo "+ autogen.sh done."
   exit 0
-}
+fi
 
 echo "+ running configure ... "
 test ! -z "$CONFIGURE_DEF_OPT" && echo "  ./configure default flags: $CONFIGURE_DEF_OPT"
 test ! -z "$CONFIGURE_EXT_OPT" && echo "  ./configure external flags: $CONFIGURE_EXT_OPT"
 echo
 
-./configure $CONFIGURE_DEF_OPT $CONFIGURE_EXT_OPT || {
-        echo "  configure failed"
-        exit 1
-}
+./configure $CONFIGURE_DEF_OPT $CONFIGURE_EXT_OPT || exit 2
 
 # install pre-commit hook for doing clean commits
 rm -f .git/hooks/pre-commit
