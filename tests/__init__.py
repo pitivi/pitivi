2d96880f090fea5aa866164d62f02cc7fc95b7d3
Thibault Saunier <tsaunier@gnome.org>
2016-07-09 08:58:20 -0400

    Make ptvenv --check run make check
    And fix make check running when pitivi has not bin built

--- tests/__init__.py
+++ tests/__init__.py
@@ -15,48 +15,39 @@ def get_pitivi_dir():
     return os.path.abspath(pitivi_dir)
 
 
-def get_build_dir():
-    """Gets the build directory."""
-    from pitivi.configure import in_devel
-    if in_devel():
-        # It's the same.
-        build_dir = get_pitivi_dir()
-    else:
-        # Probably running make distcheck. The path to the test files
-        # is different than the build path, so we must use the current
-        # dir which is build_path/tests.
-        build_dir = os.path.join(
-            os.path.abspath(os.path.curdir), os.path.pardir)
-    return os.path.abspath(build_dir)
-
-
-def _prepend_env_path(name, value):
+def _prepend_env_paths(**args):
     """Prepends one or more paths to an environment variable."""
-    os.environ[name] = os.pathsep.join(value +
-                                       os.environ.get(name, "").split(
-                                           os.pathsep))
+
+    for name, value in args.items():
+        if not isinstance(value, list):
+            value = [value]
+
+        os.environ[name] = os.pathsep.join(
+            value + os.environ.get(name, "").split(
+                os.pathsep))
 
 
 def setup():
     """Sets paths and initializes modules, to be able to run the tests."""
     res = True
+
     # Make available to configure.py the top level dir.
     pitivi_dir = get_pitivi_dir()
     sys.path.insert(0, pitivi_dir)
 
-    os.environ.setdefault('PITIVI_TOP_LEVEL_DIR', pitivi_dir)
-
-    _prepend_env_path("GST_PRESET_PATH", [
-        os.path.join(pitivi_dir, "data", "videopresets"),
-        os.path.join(pitivi_dir, "data", "audiopresets")])
-
-    _prepend_env_path("GST_ENCODING_TARGET_PATH", [
-        os.path.join(pitivi_dir, "data", "encoding-profiles")])
+    from pitivi import configure
 
     # Make available the compiled C code.
-    build_dir = get_build_dir()
-    libs_dir = os.path.join(build_dir, "pitivi/coptimizations/.libs")
-    sys.path.append(libs_dir)
+    sys.path.append(configure.BUILDDIR)
+    subproject_paths = os.path.join(configure.BUILDDIR, "subprojects", "gst-transcoder")
+
+    _prepend_env_paths(LD_LIBRARY_PATH=subproject_paths,
+                       GST_PLUGIN_PATH=subproject_paths,
+                       GI_TYPELIB_PATH=subproject_paths,
+                       GST_PRESET_PATH=[os.path.join(pitivi_dir, "data", "videopresets"),
+                                        os.path.join(pitivi_dir, "data", "audiopresets")],
+                       GST_ENCODING_TARGET_PATH=[os.path.join(pitivi_dir, "data", "encoding-profiles")])
+    os.environ.setdefault('PITIVI_TOP_LEVEL_DIR', pitivi_dir)
 
     # Make sure the modules are initialized correctly.
     from pitivi import check
