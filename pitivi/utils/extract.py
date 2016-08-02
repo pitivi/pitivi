967a381909f8a716c3dfdecb270e6f764b8b5aa5
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2016-08-02 13:01:27 +0200

    utils: Remove import from comment
    Reviewed-by: Alex Băluț <alexandru.balut@gmail.com>
    Differential Revision: https://phabricator.freedesktop.org/D716

--- pitivi/utils/extract.py
+++ pitivi/utils/extract.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this program; if not, write to the
 # Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
-# Boston, MA 02110-1301, USA.from gi.repository import Gst
+# Boston, MA 02110-1301, USA.
 """
 Classes for extracting decoded contents of streams into Python
 
