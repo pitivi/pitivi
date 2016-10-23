pitivi/pitivi

commit b68ce73bf3718e011c7cb6c084c26ccac2afae7c

medialibrary: Limit the size of the small thumbnails
Reviewed-by: Thibault Saunier <tsaunier@gnome.org>
Differential Revision: https://phabricator.freedesktop.org/D1357

pitivi/medialibrary.py
@@ -246,6 +246,11 @@ class AssetThumbnail(Loggable):
                             LARGE_THUMB_WIDTH,
                             LARGE_THUMB_WIDTH * height / width,
                             GdkPixbuf.InterpType.BILINEAR)
+                        if large_thumb.props.width > SMALL_THUMB_WIDTH:
+                            small_thumb = small_thumb.scale_simple(
+                                SMALL_THUMB_WIDTH,
+                                SMALL_THUMB_WIDTH * height / width,
+                                GdkPixbuf.InterpType.BILINEAR)
         else:
             small_thumb, large_thumb = self.__get_icons("audio-x-generic")
         return small_thumb, large_thumb

commit 000aa3a637987c289f5573fff86cb57aaecc2bf5

medialibrary: Fix check for small_thumb shrinking
Should have been part of b68ce73bf3718e011c7cb6c084c26ccac2afae7c
Broke during a rebase.

Reviewed-by: Thibault Saunier <tsaunier@gnome.org>
Differential Revision: https://phabricator.freedesktop.org/D1372

pitivi/medialibrary.py
@@ -231,7 +231,7 @@ class AssetThumbnail(Loggable):
                             LARGE_THUMB_WIDTH,
                             LARGE_THUMB_WIDTH * height / width,
                             GdkPixbuf.InterpType.BILINEAR)
-                        if large_thumb.props.width > SMALL_THUMB_WIDTH:
+                        if width > SMALL_THUMB_WIDTH:
                             small_thumb = small_thumb.scale_simple(
                                 SMALL_THUMB_WIDTH,
                                 SMALL_THUMB_WIDTH * height / width,