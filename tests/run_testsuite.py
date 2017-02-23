92ed9f2f34a90d70436fb95b7a1238850e780cc6
Thibault Saunier <tsaunier@gnome.org>
2017-02-23 09:15:50 -0300

    tests: Base our testsuite on gst-validate-launcher
    It brings in a lot of feature such as segfault detection and backtrace printing
    generation of xunit files, ability to run in gdb or valgrind
    etc...
    
    Remove the option to generate the xunit file as the user should simply
    run gst-validate-launcher tests/run_testsuite.py --xunit-file=file.xunit
    to generate it, no reason to have it in the build system anymore.
    
    Differential Revision: https://phabricator.freedesktop.org/D1677

---
+++ tests/run_testsuite.py
@@ -0,0 +1,59 @@
+# Pitivi video editor
+# Copyright (c) 2017, Thibault Saunier <tsaunier@gnome.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+# pylint: disable=missing-docstring,invalid-name
+"""GstValidateLauncher testsuite to run out unit tests."""
+import os
+import sys
+import unittest
+
+TEST_MANAGER = "base"
+CDIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(CDIR, '..'))
+
+# pylint: disable=wrong-import-position
+# pylint: disable=unused-import
+# Import tests so that the module is initialized
+import tests  # noqa
+# pylint: disable=import-error
+from launcher.baseclasses import Test  # noqa
+
+
+# pylint: disable=too-few-public-methods
+class PitiviTest(Test):
+    """A launcher.Test subclass for our unit tests."""
+    def build_arguments(self):
+        """Builds subprocess arguments."""
+        self.add_arguments('-m', 'unittest', self.classname)
+
+
+def setup_tests(test_manager, options):
+    """Sets up Pitivi unit testsuite."""
+    print("Forcing 1 job at a time as testsuite will fail otherwise")
+    options.num_jobs = 1
+    loader = unittest.TestLoader()
+    testsuites = loader.discover(CDIR)
+    for testsuite in testsuites:
+        for _tests in testsuite:
+            for test in _tests:
+                test_manager.add_test(PitiviTest(
+                    sys.executable,
+                    'tests.' + test.id(),
+                    options, test_manager.reporter,
+                    extra_env_variables={'PYTHONPATH': os.path.join(CDIR, '..')}))
+
+    return True
