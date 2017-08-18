pitivi/pitivi

commit 536e99c73aa9cda5cbbc2e6eeabcdd9ebb6d8dfd

elements: Lazy create the KeyFrame curves
Creating the UI representation of the keyframe is expensive (on big
projects it can take several seconds to create them - 25s for the
project provided in T7793).

We do not need to create them when creating the clip UI but only once
the clip is selected, so make sure to create them until actually
requred.

Fixes T7793

Reviewed-by: Alex Băluț <<alexandru.balut@gmail.com>>
Differential Revision: https://phabricator.freedesktop.org/D1823

pitivi/timeline/elements.py
@@ -506,7 +506,8 @@ class TimelineElement(Gtk.Layout, Zoomable, Loggable):
         self.keyframe_curve = None
 
     # Private methods
-    def __createKeyframeCurve(self, binding):
+    def __ensure_keyframes(self, binding):
+        """Ensures that we have at least 2 keyframes (at inpoint and end)."""
         source = binding.props.control_source
         values = source.get_all()
 
@@ -519,7 +520,10 @@ class TimelineElement(Gtk.Layout, Zoomable, Loggable):
             assert source.set(inpoint, val)
             assert source.set(inpoint + self._ges_elem.props.duration, val)
 
+    def __create_keyframe_curve(self):
+        """Creates required keyframe curve."""
         self.__removeKeyframes()
+        binding = self._ges_elem.get_control_binding(self.__controlledProperty.name)
         self.keyframe_curve = KeyframeCurve(self.timeline, binding)
         self.keyframe_curve.connect("enter", self.__curveEnterCb)
         self.keyframe_curve.connect("leave", self.__curveLeaveCb)
@@ -528,6 +532,7 @@ class TimelineElement(Gtk.Layout, Zoomable, Loggable):
         self.__update_keyframe_curve_visibility()
 
     def __createControlBinding(self, element):
+        """Creates the required ControlBinding and keyframes."""
         if self.__controlledProperty:
             element.connect("control-binding-added",
                             self.__controlBindingAddedCb)
@@ -535,7 +540,7 @@ class TimelineElement(Gtk.Layout, Zoomable, Loggable):
                 element.get_control_binding(self.__controlledProperty.name)
 
             if binding:
-                self.__createKeyframeCurve(binding)
+                self.__ensure_keyframes(binding)
 
                 return
 
@@ -546,7 +551,7 @@ class TimelineElement(Gtk.Layout, Zoomable, Loggable):
 
     def __controlBindingAddedCb(self, unused_ges_elem, binding):
         if binding.props.name == self.__controlledProperty.name:
-            self.__createKeyframeCurve(binding)
+            self.__ensure_keyframes(binding)
 
     def do_draw(self, cr):
         self.propagate_draw(self.__background, cr)
@@ -561,6 +566,9 @@ class TimelineElement(Gtk.Layout, Zoomable, Loggable):
 
     # Callbacks
     def __selectedChangedCb(self, unused_selected, selected):
+        if not self.keyframe_curve and self.__controlledProperty:
+            self.__create_keyframe_curve()
+
         if self.keyframe_curve:
             self.__update_keyframe_curve_visibility()
 

commit b30fc128c0712aa5dc4d98839e4460ab0234c042

elements: Displaying effects keyframe curves when required
This was a regression introduced in 536e99c73aa9cda5cbbc2e6eeabcdd9ebb6d8dfd
"elements: Lazy create the KeyFrame curves".

Fixes T7812

Reviewed-by: Stefan-Adrian Popa <stefanpopa2209@gmail.com>
Differential Revision: https://phabricator.freedesktop.org/D1850

pitivi/timeline/elements.py
@@ -452,7 +452,7 @@ class TimelineElement(Gtk.Layout, Zoomable, Loggable):
         # We set up the default mixing property right here, if a binding was
         # already set (when loading a project), it will be added later
         # and override that one.
-        self.showDefaultKeyframes()
+        self.showDefaultKeyframes(lazy_render=True)
 
     def release(self):
         if self.__previewer:
@@ -477,9 +477,12 @@ class TimelineElement(Gtk.Layout, Zoomable, Loggable):
 
     def showKeyframes(self, ges_elem, prop):
         self.__setKeyframes(ges_elem, prop)
+        self.__create_keyframe_curve(ges_elem)
 
-    def showDefaultKeyframes(self):
+    def showDefaultKeyframes(self, lazy_render=False):
         self.__setKeyframes(self._ges_elem, self._getDefaultMixingProperty())
+        if not lazy_render:
+            self.__create_keyframe_curve()
 
     def __setKeyframes(self, ges_elem, prop):
         self.__removeKeyframes()
@@ -520,10 +523,13 @@ class TimelineElement(Gtk.Layout, Zoomable, Loggable):
             assert source.set(inpoint, val)
             assert source.set(inpoint + self._ges_elem.props.duration, val)
 
-    def __create_keyframe_curve(self):
+    def __create_keyframe_curve(self, ges_elem=None):
         """Creates required keyframe curve."""
         self.__removeKeyframes()
-        binding = self._ges_elem.get_control_binding(self.__controlledProperty.name)
+        if not ges_elem:
+            ges_elem = self._ges_elem
+
+        binding = ges_elem.get_control_binding(self.__controlledProperty.name)
         self.keyframe_curve = KeyframeCurve(self.timeline, binding)
         self.keyframe_curve.connect("enter", self.__curveEnterCb)
         self.keyframe_curve.connect("leave", self.__curveLeaveCb)
@@ -578,7 +584,8 @@ class TimelineElement(Gtk.Layout, Zoomable, Loggable):
     def __update_keyframe_curve_visibility(self):
         """Updates the keyframes widget visibility by adding or removing it."""
         if self._ges_elem.selected and len(self.timeline.selection) == 1:
-            self.add(self.keyframe_curve)
+            if not self.keyframe_curve.get_parent():
+                self.add(self.keyframe_curve)
         else:
             self.remove(self.keyframe_curve)
 