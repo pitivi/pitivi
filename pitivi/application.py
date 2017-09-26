ddaf74999b40b450255bca89e19da32affa9f374
Thibault Saunier <tsaunier@gnome.org>
2017-09-26 09:07:17 -0300

    application: Fix dbus app_id to match flatpak expectations

--- pitivi/application.py
+++ pitivi/application.py
@@ -69,7 +69,7 @@ class Pitivi(Gtk.Application, Loggable):
 
     def __init__(self):
         Gtk.Application.__init__(self,
-                                 application_id="org.pitivi",
+                                 application_id="org.pitivi.Pitivi",
                                  flags=Gio.ApplicationFlags.HANDLES_OPEN)
         Loggable.__init__(self)
 
