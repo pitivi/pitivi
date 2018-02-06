a4c70c0369e5b2d1a926a7f0ff5f5ec0fe28010a
Alexandru Băluț <alexandru.balut@gmail.com>
2018-02-06 22:29:33 +0100

    pipeline: Remove unused field
    It was introduced in commit f04cdaf204ea664933ea524057158fae291c66f6 but
    it has never been used.

--- pitivi/utils/pipeline.py
+++ pitivi/utils/pipeline.py
@@ -613,7 +613,6 @@ class Pipeline(GES.Pipeline, SimplePipeline):
 
     def _busMessageCb(self, bus, message):
         if message.type == Gst.MessageType.ASYNC_DONE:
-            self.commiting = False
             self.app.gui.timeline_ui.timeline.update_visible_overlays()
 
         if message.type == Gst.MessageType.ASYNC_DONE and\
