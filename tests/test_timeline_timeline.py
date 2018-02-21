5f503f136c5391d6b3d10572b5ab57375710d038
Harish Fulara <harish14143@iiitd.ac.in>
2018-02-22 01:40:11 +0530

    [FIX] Paste copied clip(s) multiple times
    How copy-paste works? The object (o_copy) returned by the copy function stores reference to the object (o_orig) whose copy is made. While pasting o_copy onto the timeline, this reference is used to set the timeline for o_copy, i.e., timeline(o_copy) = timeline(o_orig), which makes o_copy get pasted on the timeline.
    
    Issue with previous logic? We were saving a copy of 'self.__copiedGroup' before pasting it onto the timeline and then restoring 'self.__copiedGroup' from previously saved copy after the paste operation is done (for the first time). This saving and restoring 'self.__copiedGroup' would make it store a reference to itself. When we paste again (for the second time) on the timeline, no timeline would be associated with the pasted object because 'self.__copiedGroup' refers to itself and there is no timeline associated with 'self.__copiedGroup'. Hence, nothing would get pasted onto the timeline.

--- tests/test_timeline_timeline.py
+++ tests/test_timeline_timeline.py
@@ -475,6 +475,7 @@ class TestCopyPaste(BaseTestTimeline):
         clips = layer.get_clips()
         self.assertEqual(len(clips), 2)
 
+        # Pasting clips for the first time.
         timeline_container.paste_action.emit("activate", None)
 
         n_clips = layer.get_clips()
@@ -484,6 +485,19 @@ class TestCopyPaste(BaseTestTimeline):
         self.assertEqual(len(copied_clips), 2)
         self.assertEqual(copied_clips[0].props.start, position)
         self.assertEqual(copied_clips[1].props.start, position + 10)
+
+        # Pasting same clips second time.
+        position = 40
+        project.pipeline.getPosition = mock.Mock(return_value=position)
+        timeline_container.paste_action.emit("activate", None)
+
+        n_clips = layer.get_clips()
+        self.assertEqual(len(n_clips), 6)
+
+        copied_clips = [clip for clip in n_clips if clip not in clips]
+        self.assertEqual(len(copied_clips), 4)
+        self.assertEqual(copied_clips[2].props.start, position)
+        self.assertEqual(copied_clips[3].props.start, position + 10)
 
 
 class TestEditing(BaseTestTimeline):
