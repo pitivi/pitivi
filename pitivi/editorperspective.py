cc1502a32b4280f574e26360033bb468b0763771
HarishFulara07 <harish14143@iiitd.ac.in>
2018-06-23 16:20:49 +0530

    project: Remove project title functionality

--- pitivi/editorperspective.py
+++ pitivi/editorperspective.py
@@ -232,8 +232,6 @@ class EditorPerspective(Perspective, Loggable):
         self.mainhpaned.set_position(self.settings.mainWindowMainHPanePosition)
         self.toplevel_widget.set_position(self.settings.mainWindowVPanePosition)
 
-        self.updateTitle()
-
     def _setDefaultPositions(self):
         window_width = self.app.gui.get_size()[0]
         if self.settings.mainWindowHPanePosition is None:
@@ -597,7 +595,6 @@ class EditorPerspective(Perspective, Loggable):
             project.pipeline.deactivatePositionListener()
 
         self.info("Project closed")
-        self.updateTitle()
         if project.loaded:
             self._disconnectFromProject(project)
         self.timeline_ui.setProject(None)
@@ -736,12 +733,8 @@ class EditorPerspective(Perspective, Loggable):
         asset = GES.Formatter.get_default()
         asset_extension = asset.get_meta(GES.META_FORMATTER_EXTENSION)
 
-        if not project.name:
-            chooser.set_current_name(
-                _("Untitled") + "." + asset_extension + "_tar")
-        else:
-            chooser.set_current_name(
-                project.name + "." + asset_extension + "_tar")
+        chooser.set_current_name(
+            project.name + "." + asset_extension + "_tar")
 
         filt = Gtk.FileFilter()
         filt.set_name(_("Tar archive"))
@@ -860,60 +853,13 @@ class EditorPerspective(Perspective, Loggable):
     def updateTitle(self):
         project = self.app.project_manager.current_project
         if project:
-            if project.name:
-                name = project.name
-            else:
-                name = _("Untitled")
             unsaved_mark = ""
             if project.hasUnsavedModifications():
                 unsaved_mark = "*"
-            title = "%s%s — %s" % (unsaved_mark, name, APPNAME)
+            title = "%s%s — %s" % (unsaved_mark, project.name, APPNAME)
         else:
             title = APPNAME
-        event_box = Gtk.EventBox()
-        label = Gtk.Label()
-        clear_styles(label)
-        label.set_text(title)
-        event_box.add(label)
-        event_box.show_all()
-        event_box.connect("button-press-event", self.__titleClickCb, project)
-        self.headerbar.set_custom_title(event_box)
-        self.app.gui.set_title(title)
-
-    def __titleClickCb(self, unused_widget, unused_event, project):
-        entry = Gtk.Entry()
-        entry.set_width_chars(100)
-        entry.set_margin_left(SPACING)
-        entry.set_margin_right(SPACING)
-        entry.show()
-        entry.set_text(project.name)
-        self.headerbar.set_custom_title(entry)
-        if project.hasDefaultName():
-            entry.grab_focus()
-        else:
-            entry.grab_focus_without_selecting()
-        entry.connect("focus-out-event", self.__titleChangedCb, project)
-        entry.connect("key_release_event", self.__titleTypeCb, project)
-
-    def __titleChangedCb(self, widget, event, project):
-        if not event.window:
-            # Workaround https://bugzilla.gnome.org/show_bug.cgi?id=757036
-            return
-        name = widget.get_text()
-        if project.name == name:
-            self.updateTitle()
-        else:
-            project.name = name
-
-    def __titleTypeCb(self, widget, event, project):
-        if event.keyval == Gdk.KEY_Return:
-            self.focusTimeline()
-            return True
-        elif event.keyval == Gdk.KEY_Escape:
-            widget.set_text(project.name)
-            self.focusTimeline()
-            return True
-        return False
+        self.headerbar.set_title(title)
 
 
 class PreviewAssetWindow(Gtk.Window):
