28c289d90f92fad4073b2ee49daca479a30941f2
Alexandru Băluț <alexandru.balut@gmail.com>
2019-01-14 23:15:16 +0100

    pipeline: Remove unneeded fields

--- pitivi/mediafilespreviewer.py
+++ pitivi/mediafilespreviewer.py
@@ -102,7 +102,8 @@ class PreviewWidget(Gtk.Grid, Loggable):
 
         # Gui elements:
         # Drawing area for video output
-        self.preview_video = ViewerWidget(self.player.sink_widget)
+        unused_sink, sink_widget = self.player.create_sink()
+        self.preview_video = ViewerWidget(sink_widget)
         self.preview_video.props.hexpand = minimal
         self.preview_video.props.vexpand = minimal
         self.preview_video.show_all()
