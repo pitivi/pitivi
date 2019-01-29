2be08f64f7c2ef368548b0b2d93743bd68b4200d
Alexandru Băluț <alexandru.balut@gmail.com>
2019-01-29 07:01:31 +0100

    widgets: Fix setting the default value of the widget

--- pitivi/utils/widgets.py
+++ pitivi/utils/widgets.py
@@ -1112,7 +1112,7 @@ class GstElementSettingsWidget(Gtk.Box, Loggable):
 
         if value is None:
             value = prop.default_value
-        else:
+        if value is not None:
             widget.setWidgetValue(value)
 
         return widget
