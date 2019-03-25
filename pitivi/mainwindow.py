5981e9632956a8d2b76201747341a371c6a53b33
swayne <swayamjeet swain>
2019-03-25 20:45:13 +0530

    Fixes #2287-mainwindow: Add Preferences keyboard shortcut
    changed default primary key to ctrl+,
    
    Fixes #2287 - mainwindow: Add Preferences keyboard shortcut
    
    Fixes #2287-mainwindow: Add Preferences keyboard shortcut
    
    added preferences keyboard shortcut
    
    changed default primary key to ctrl+,
    
    Fixes #2287-mainwindow: Add Preferences keyboard shortcut
    
    Fixes #2287-mainwindow: Add Preferences keyboard shortcut
    
    Fixes #2287-mainwindow: Add Preferences keyboard shortcut

--- pitivi/mainwindow.py
+++ pitivi/mainwindow.py
@@ -174,6 +174,17 @@ class MainWindow(Gtk.ApplicationWindow, Loggable):
         self.preferences_action = Gio.SimpleAction.new("preferences", None)
         self.preferences_action.connect("activate", self.__preferences_cb)
         self.add_action(self.preferences_action)
+<<<<<<< HEAD
+<<<<<<< HEAD
+        self.app.shortcuts.add("win.preferences", ["<Primary>comma"], _("Preferences"), group="app")
+        
+
+=======
+        self.app.shortcuts.add("win.preferences", ["F9"], _("Preferences"), group="app")
+>>>>>>> 4e27544c... added preferences keyboard shortcut
+=======
+        self.app.shortcuts.add("win.preferences", ["<Primary>comma"], _("Preferences"), group="app")
+>>>>>>> eee01375... changed default primary key to ctrl+,
 
     @staticmethod
     def __user_manual_cb(unused_action, unused_param):
