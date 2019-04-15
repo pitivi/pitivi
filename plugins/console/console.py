88600b9b7245c957b2aeb82033ec1192870e4c36
swayne <swayamjeet swain>
2019-04-15 16:19:13 +0530

    plugins: Assign a default shortcut for the console
    Fixes #2291

--- plugins/console/console.py
+++ plugins/console/console.py
@@ -161,7 +161,8 @@ class Console(GObject.GObject, Peas.Activatable):
         open_action = Gio.SimpleAction.new("open_console", None)
         open_action.connect("activate", self.__menu_item_activate_cb)
         self.app.add_action(open_action)
-
+        self.app.shortcuts.add("app.open_console", ["<Primary>d"], _("Developer Console"))
+        
         self._setup_dialog()
         self.add_menu_item()
         self.menu_item.show()
