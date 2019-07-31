8d1f76445f0851b4562c3d3833dd806f8e2e7fb8
Ayush <ayush.mittal9398@gmail.com>
2019-08-01 00:11:58 +0530

    timeline: Allow seeking to previous/next cut
    Fixes #2281

--- tests/test_timeline_timeline.py
+++ tests/test_timeline_timeline.py
@@ -798,3 +798,27 @@ class TestTimelineContainer(BaseTestTimeline):
 
         # Check the title clips are ignored.
         timeline_container.update_clips_asset(mock.Mock(), mock.Mock())
+
+
+class TestClipsEdges(BaseTestTimeline):
+
+    def test_clips_edges(self):
+        """Test function for function clips_edges."""
+        timeline_container = common.create_timeline_container()
+        timeline = timeline_container.timeline
+        clips = self.addClipsSimple(timeline, 5)
+        timeline.ges_timeline.layers[0].remove_clip(clips[0])
+        self.add_clip(timeline.ges_timeline.layers[0], 5, 0, 10)
+
+        self.assertEqual(timeline_container.first_clip_edge(after=0), 5)
+        self.assertEqual(timeline_container.first_clip_edge(after=9), 10)
+        self.assertEqual(timeline_container.first_clip_edge(after=10), 15)
+        self.assertEqual(timeline_container.first_clip_edge(after=48), 50)
+        self.assertEqual(timeline_container.first_clip_edge(after=49), 50)
+
+        self.assertEqual(timeline_container.first_clip_edge(before=0), None)
+        self.assertEqual(timeline_container.first_clip_edge(before=1), 0)
+        self.assertEqual(timeline_container.first_clip_edge(before=9), 5)
+        self.assertEqual(timeline_container.first_clip_edge(before=10), 5)
+        self.assertEqual(timeline_container.first_clip_edge(before=11), 10)
+        self.assertEqual(timeline_container.first_clip_edge(before=20), 15)
