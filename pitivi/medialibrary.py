45d29c73c7c71f91b56d4b69abd8ed0b86f6ba93
Swayamjeet <swayam1998@gmail.com>
2019-08-14 23:57:02 +0530

    Editing Nested Timelines by starting up another Pitivi instance

--- pitivi/medialibrary.py
+++ pitivi/medialibrary.py
@@ -19,6 +19,8 @@
 # Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
 # Boston, MA 02110-1301, USA.
 import os
+import subprocess
+import sys
 import time
 from gettext import gettext as _
 from gettext import ngettext
@@ -796,6 +798,9 @@ class MediaLibraryWidget(Gtk.Box, Loggable):
         filter.add_custom(Gtk.FileFilterFlags.URI |
                           Gtk.FileFilterFlags.MIME_TYPE,
                           self.__filter_unsupported)
+        for formatter in GES.list_assets(GES.Formatter):
+            for extension in formatter.get_meta('extension').split(','):
+                filter.add_pattern("*.%s" % extension)
         dialog.add_filter(filter)
 
         # ...and allow the user to override our whitelists
@@ -824,6 +829,14 @@ class MediaLibraryWidget(Gtk.Box, Loggable):
 
         if self._project.loaded:
             self._flushPendingAssets()
+
+    def update_nested_clip_thumbs(self, assets):
+        for asset in assets:
+            for row in self.storemodel:
+                if asset == row[COL_ASSET]:
+                    row[COL_THUMB_DECORATOR].previewer = None
+                    row[COL_THUMB_DECORATOR].refresh()
+                    break
 
     def _flushPendingAssets(self):
         self.debug("Flushing %d pending model rows", len(self._pending_assets))
@@ -1226,6 +1239,13 @@ class MediaLibraryWidget(Gtk.Box, Loggable):
         parent_path = os.path.dirname(path_from_uri(assets[0].get_id()))
         Gio.AppInfo.launch_default_for_uri(Gst.filename_to_uri(parent_path), None)
 
+    def __edit_nested_clip_cb(self, unused_action, unused_parameter):
+        assets = self.getSelectedAssets()
+        if len(assets) != 1:
+            return
+        parent_path = os.path.abspath(path_from_uri(assets[0].get_id()))
+        subprocess.Popen([sys.argv[0], parent_path])
+
     def __createMenuModel(self):
         if self.app.proxy_manager.proxyingUnsupported:
             return None, None
@@ -1242,6 +1262,13 @@ class MediaLibraryWidget(Gtk.Box, Loggable):
             action.connect("activate", self.__open_containing_folder_cb)
             action_group.insert(action)
             text = _("Open containing folder")
+            menu_model.append(text, "assets.%s" % action.get_name().replace(" ", "."))
+
+        if len(assets) == 1 and assets[0].props.is_nested_timeline:
+            action = Gio.SimpleAction.new("edit-nested-clip", None)
+            action.connect("activate", self.__edit_nested_clip_cb)
+            action_group.insert(action)
+            text = _("Edit")
             menu_model.append(text, "assets.%s" % action.get_name().replace(" ", "."))
 
         image_assets = [asset for asset in assets
