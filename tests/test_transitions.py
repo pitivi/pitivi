395b262f3d4840fc2f0ddda8a9d048ccf573900d
Alexandru Băluț <alexandru.balut@gmail.com>
2019-09-21 02:43:14 +0200

    transitions: Fix transition types detection
    Fixes #2352

---
+++ tests/test_transitions.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Pitivi video editor
+# Copyright (c) 2019, Alex Băluț <alexandru.balut@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+"""Tests for the transitions module."""
+from pitivi.transitions import TransitionsListWidget
+from tests import common
+
+
+class TransitionsListWidgetTest(common.TestCase):
+    """Tests for the TransitionsListWidget class."""
+
+    def test_transition_types_loaded(self):
+        """Checks the transition types are properly detected."""
+        app = common.create_pitivi_mock()
+        widget = TransitionsListWidget(app)
+        mainloop = common.create_main_loop()
+        mainloop.run(until_empty=True)
+        self.assertGreater(len(widget.storemodel), 10)
