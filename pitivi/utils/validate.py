edf239dea730bef72f9f0a9c27db77240f0bf4b2
Alexandru Băluț <alexandru.balut@gmail.com>
2019-10-29 17:23:01 +0100

    Fix pylint protected-access

--- pitivi/utils/validate.py
+++ pitivi/utils/validate.py
@@ -16,6 +16,7 @@
 # License along with this program; if not, write to the
 # Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
 # Boston, MA 02110-1301, USA.
+# pylint: disable=protected-access
 import locale
 import subprocess
 from unittest import mock
