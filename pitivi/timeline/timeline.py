pitivi/pitivi

commit 3761fdbf3a158f35bb87fb168153ccd94747cc43

Fix pylint attribute-defined-outside-init

pitivi/timeline/timeline.py
@@ -128,16 +128,14 @@ class Marquee(Gtk.Box, Loggable):
         Loggable.__init__(self)
 
         self._timeline = timeline
+        self.start_x, self.start_y = 0, 0
+        self.end_x, self.end_y = self.start_x, self.start_y
         self.hide()
 
         self.get_style_context().add_class("Marquee")
 
     def hide(self):
         """Hides and resets the widget."""
-        self.start_x = None
-        self.start_y = None
-        self.end_x = None
-        self.end_y = None
         self.props.height_request = -1
         self.props.width_request = -1
         self.set_visible(False)
@@ -152,6 +150,7 @@ class Marquee(Gtk.Box, Loggable):
         event_widget = Gtk.get_event_widget(event)
         self.start_x, self.start_y = event_widget.translate_coordinates(
             self._timeline.layout.layers_vbox, event.x, event.y)
+        self.end_x, self.end_y = self.start_x, self.start_y
 
     def move(self, event):
         """Sets the second corner of the marquee.
@@ -185,8 +184,8 @@ class Marquee(Gtk.Box, Loggable):
         start_pos = max(0, self._timeline.pixelToNs(self.start_x))
         end_pos = max(0, self._timeline.pixelToNs(self.end_x))
 
-        return self._timeline.get_clips_in_between(start_layer,
-                                                   end_layer, start_pos, end_pos)
+        return self._timeline.get_clips_in_between(start_layer, end_layer,
+                                                   start_pos, end_pos)
 
 
 class LayersLayout(Gtk.Layout, Zoomable, Loggable):
@@ -736,6 +735,7 @@ class Timeline(Gtk.EventBox, Zoomable, Loggable):
 
         self._scrolling = res and button == 2
         if self._scrolling:
+            # pylint: disable=attribute-defined-outside-init
             self._scroll_start_x = event.x
             self._scroll_start_y = event.y
 
@@ -1420,8 +1420,8 @@ class TimelineContainer(Gtk.Grid, Zoomable, Loggable):
         self.ges_timeline = None
         self.__copied_group = None
 
-        self._createUi()
-        self._createActions()
+        self._create_ui()
+        self._create_actions()
 
         self.timeline.connect("size-allocate", self.__timeline_size_allocate_cb)
 
@@ -1555,7 +1555,7 @@ class TimelineContainer(Gtk.Grid, Zoomable, Loggable):
         if project:
             self.ruler.setPipeline(project.pipeline)
             self.ruler.zoomChanged()
-            self._update_ruler(project.videorate)
+            self.ruler.setProjectFrameRate(project.videorate)
 
             self.timeline.set_best_zoom_ratio(allow_zoom_in=True)
             self.timeline.update_snapping_distance()
@@ -1580,7 +1580,7 @@ class TimelineContainer(Gtk.Grid, Zoomable, Loggable):
 
     # Internal API
 
-    def _createUi(self):
+    def _create_ui(self):
         left_size_group = Gtk.SizeGroup.new(Gtk.SizeGroupMode.HORIZONTAL)
         zoom_box = ZoomBox(self)
         left_size_group.add_widget(zoom_box)
@@ -1641,7 +1641,7 @@ class TimelineContainer(Gtk.Grid, Zoomable, Loggable):
         unused_longest_time, longest_layer = max(layer_lengths)
         return longest_layer
 
-    def _createActions(self):
+    def _create_actions(self):
         # The actions below are added to this action group and thus
         # are accessible only to the self.timeline.layout and self.toolbar
         # widgets (and their children) using the "timeline" prefix.
@@ -2081,11 +2081,12 @@ class TimelineContainer(Gtk.Grid, Zoomable, Loggable):
         self.timeline.scrollToPlayhead(align=Gtk.Align.CENTER, when_not_in_view=True)
 
     def _seek_backward_one_frame_cb(self, unused_action, unused_parameter):
-        self._project.pipeline.stepFrame(self._framerate, -1)
+        self._project.pipeline.stepFrame(self._project.videorate, -1)
         self.timeline.scrollToPlayhead(align=Gtk.Align.CENTER, when_not_in_view=True)
 
     def _seek_forward_one_frame_cb(self, unused_action, unused_parameter):
-        self._project.pipeline.stepFrame(self._framerate, 1)
+
+        self._project.pipeline.stepFrame(self._project.videorate, 1)
         self.timeline.scrollToPlayhead(align=Gtk.Align.CENTER, when_not_in_view=True)
 
     def do_focus_in_event(self, unused_event):
@@ -2099,11 +2100,7 @@ class TimelineContainer(Gtk.Grid, Zoomable, Loggable):
     def _rendering_settings_changed_cb(self, project, item):
         """Handles Project metadata changes."""
         if item == "videorate" or item is None:
-            self._update_ruler(project.videorate)
-
-    def _update_ruler(self, videorate):
-        self._framerate = videorate
-        self.ruler.setProjectFrameRate(self._framerate)
+            self.ruler.setProjectFrameRate(project.videorate)
 
     def __timeline_size_allocate_cb(self, unused_widget, allocation):
         fits = self.timeline.layout.props.height <= allocation.height

pitivi/utils/timeline.py
@@ -272,9 +272,10 @@ class EditingContext(GObject.Object, Loggable):
         self.old_position = self.focus.get_start()
         if edge == GES.Edge.EDGE_END and mode == GES.EditMode.EDIT_TRIM:
             self.old_position += self.focus.get_duration()
-
         self.old_priority = self.focus.get_priority()
+
         self.new_position = None
+        self.new_priority = None
 
         self.timeline = timeline
         self.app = app

pitivi/application.py
@@ -143,6 +143,7 @@ class Pitivi(Gtk.Application, Loggable):
         self._checkVersion()
 
     def _setup(self):
+        # pylint: disable=attribute-defined-outside-init
         self.settings = GlobalSettings()
         self.threads = ThreadMaster()
         self.effects = EffectsManager()
@@ -154,11 +155,13 @@ class Pitivi(Gtk.Application, Loggable):
         self.project_manager.connect_after("project-closed", self._project_closed_cb)
         self.project_manager.connect("project-saved", self.__project_saved_cb)
 
-        self._createActions()
+        self._create_actions()
         self._syncDoUndo()
 
-    def _createActions(self):
+    def _create_actions(self):
         self.shortcuts.register_group("app", _("General"), position=10)
+
+        # pylint: disable=attribute-defined-outside-init
         self.undo_action = Gio.SimpleAction.new("undo", None)
         self.undo_action.connect("activate", self._undoCb)
         self.add_action(self.undo_action)

pitivi/dialogs/prefs.py
@@ -609,6 +609,9 @@ class CustomShortcutDialog(Gtk.Dialog):
         self.preferences = pref_dialog
         self.customised_item = customised_item
 
+        # The keyboard shortcut typed by the user.
+        self.accelerator = None
+
         self.set_title(_("Set Shortcut"))
         # Set a minimum size.
         self.set_size_request(500, 300)
@@ -697,6 +700,7 @@ class CustomShortcutDialog(Gtk.Dialog):
     def do_response(self, response):
         """Handles the user's response."""
         if response == Gtk.ResponseType.OK:
+            assert self.accelerator
             if self.conflicting_action:
                 # Disable the accelerator in its previous use, set for this action.
                 accels = self.app.get_accels_for_action(self.conflicting_action)

pitivi/editorperspective.py
@@ -100,7 +100,7 @@ class EditorPerspective(Perspective, Loggable):
     def setup_ui(self):
         """Sets up the UI."""
         self.__setup_css()
-        self._createUi()
+        self._create_ui()
         self.app.gui.connect("focus-in-event", self.__focus_in_event_cb)
         self.app.gui.connect("destroy", self._destroyedCb)
 
@@ -164,7 +164,7 @@ class EditorPerspective(Perspective, Loggable):
         dialog = RenderDialog(self.app, project)
         dialog.window.show()
 
-    def _createUi(self):
+    def _create_ui(self):
         """Creates the graphical interface.
 
         The rough hierarchy is:
@@ -175,6 +175,7 @@ class EditorPerspective(Perspective, Loggable):
         The full hierarchy can be admired by starting the GTK+ Inspector
         with Ctrl+Shift+I.
         """
+        # pylint: disable=attribute-defined-outside-init
         # Main "toolbar" (using client-side window decorations with HeaderBar)
         self.headerbar = self.__create_headerbar()
 
@@ -234,7 +235,7 @@ class EditorPerspective(Perspective, Loggable):
         self.toplevel_widget.pack2(self.timeline_ui, resize=True, shrink=False)
 
         # Setup shortcuts for HeaderBar buttons and menu items.
-        self.__set_keyboard_shortcuts()
+        self._create_actions()
 
         # Identify widgets for AT-SPI, making our test suite easier to develop
         # These will show up in sniff, accerciser, etc.
@@ -312,6 +313,7 @@ class EditorPerspective(Perspective, Loggable):
         redo_button.set_action_name("app.redo")
         redo_button.set_use_underline(True)
 
+        # pylint: disable=attribute-defined-outside-init
         self.save_button = Gtk.Button.new_with_label(_("Save"))
         self.save_button.set_focus_on_click(False)
 
@@ -342,11 +344,12 @@ class EditorPerspective(Perspective, Loggable):
 
         return headerbar
 
-    def __set_keyboard_shortcuts(self):
+    def _create_actions(self):
         group = Gio.SimpleActionGroup()
         self.toplevel_widget.insert_action_group("editor", group)
         self.headerbar.insert_action_group("editor", group)
 
+        # pylint: disable=attribute-defined-outside-init
         self.save_action = Gio.SimpleAction.new("save", None)
         self.save_action.connect("activate", self.__save_project_cb)
         group.add_action(self.save_action)

pitivi/greeterperspective.py
@@ -159,7 +159,7 @@ class GreeterPerspective(Perspective):
 
         self.__setup_css()
         self.headerbar = self.__create_headerbar()
-        self.__set_keyboard_shortcuts()
+        self._create_actions()
 
     def refresh(self):
         """Refreshes the perspective."""
@@ -269,7 +269,7 @@ class GreeterPerspective(Perspective):
             else:
                 self.headerbar.set_title(_("Pitivi"))
 
-    def __set_keyboard_shortcuts(self):
+    def _create_actions(self):
         group = Gio.SimpleActionGroup()
         self.toplevel_widget.insert_action_group("greeter", group)
         self.headerbar.insert_action_group("greeter", group)

pitivi/mainwindow.py
@@ -67,7 +67,7 @@ GlobalSettings.addConfigOption('lastCurrentVersion',
                                default='')
 
 
-# pylint: disable=attribute-defined-outside-init,too-many-instance-attributes
+# pylint: disable=too-many-instance-attributes
 class MainWindow(Gtk.ApplicationWindow, Loggable):
     """Pitivi's main window.
 
@@ -112,7 +112,7 @@ class MainWindow(Gtk.ApplicationWindow, Loggable):
         self.log("Setting up the perspectives.")
 
         self.set_icon_name("pitivi")
-        self.__set_keyboard_shortcuts()
+        self._create_actions()
 
         self.greeter.setup_ui()
         self.editor.setup_ui()
@@ -163,10 +163,10 @@ class MainWindow(Gtk.ApplicationWindow, Loggable):
         self.debug("Screen size is %sx%s", screen_width, screen_height)
         return min_size.width >= 0.9 * screen_width
 
-    # pylint: disable=attribute-defined-outside-init
-    def __set_keyboard_shortcuts(self):
+    def _create_actions(self):
         self.app.shortcuts.register_group("win", _("Project"), position=20)
 
+        # pylint: disable=attribute-defined-outside-init
         self.help_action = Gio.SimpleAction.new("help", None)
         self.help_action.connect("activate", self.__user_manual_cb)
         self.add_action(self.help_action)

pitivi/medialibrary.py
@@ -270,6 +270,8 @@ class AssetThumbnail(GObject.Object, Loggable):
         self.__asset = asset
         self.proxy_manager = proxy_manager
         self.__previewer = None
+        self.small_thumb = None
+        self.large_thumb = None
         self.refresh()
 
     def refresh(self):
@@ -415,6 +417,7 @@ class AssetThumbnail(GObject.Object, Loggable):
         return icon
 
     def _set_state(self):
+        # pylint: disable=attribute-defined-outside-init
         asset = self.__asset
         target = asset.get_proxy_target()
         target_is_valid = target and not target.get_error()

commit afa54b0684885083fc7687d939f5a4fabdcc90e9

timeline: Fix selection marquee forever visible
When the mouse cursor moves, instead of checking for marquee.start_x to
determine we should update the marquee, we now check whether the marquee
is visible.

The change in Marquee.hide in 3761fdbf3a158f35bb87fb168153ccd94747cc43
broke it.

pitivi/timeline/timeline.py
@@ -130,12 +130,15 @@ class Marquee(Gtk.Box, Loggable):
         self._timeline = timeline
         self.start_x, self.start_y = 0, 0
         self.end_x, self.end_y = self.start_x, self.start_y
+
+        self.props.no_show_all = True
         self.hide()
 
         self.get_style_context().add_class("Marquee")
 
     def hide(self):
         """Hides and resets the widget."""
+        self.start_x, self.start_y = 0, 0
         self.props.height_request = -1
         self.props.width_request = -1
         self.set_visible(False)
@@ -152,10 +155,12 @@ class Marquee(Gtk.Box, Loggable):
             self._timeline.layout.layers_vbox, event.x, event.y)
         self.end_x, self.end_y = self.start_x, self.start_y
 
+        self.props.width_request = 0
+        self.props.height_request = 0
+        self.set_visible(True)
+
     def move(self, event):
         """Sets the second corner of the marquee.
-
-        Also makes the marquee visible.
 
         Args:
             event (Gdk.EventMotion): The motion event which contains
@@ -167,11 +172,10 @@ class Marquee(Gtk.Box, Loggable):
 
         x = min(self.start_x, self.end_x)
         y = min(self.start_y, self.end_y)
-
         self.get_parent().move(self, x, y)
+
         self.props.width_request = abs(self.start_x - self.end_x)
         self.props.height_request = abs(self.start_y - self.end_y)
-        self.set_visible(True)
 
     def find_clips(self):
         """Finds the clips which intersect the marquee.
@@ -179,6 +183,9 @@ class Marquee(Gtk.Box, Loggable):
         Returns:
             List[GES.Clip]: The clips under the marquee.
         """
+        if self.props.width_request == 0:
+            return []
+
         start_layer = self._timeline.get_layer_at(self.start_y)[0]
         end_layer = self._timeline.get_layer_at(self.end_y)[0]
         start_pos = max(0, self._timeline.pixel_to_ns(self.start_x))
@@ -749,7 +756,9 @@ class Timeline(Gtk.EventBox, Zoomable, Loggable):
             self.__end_moving_layer()
             return False
         elif res and button == 1:
-            self._select_under_marquee()
+            clips = self.layout.marquee.find_clips()
+            self.selection.set_selection(clips, SELECT)
+            self.layout.marquee.hide()
 
         self.scrubbing = False
 
@@ -865,7 +874,7 @@ class Timeline(Gtk.EventBox, Zoomable, Loggable):
             if layer != self.__moving_layer:
                 priority = layer.get_priority()
                 self.move_layer(self.__moving_layer, priority)
-        elif self.layout.marquee.start_x:
+        elif self.layout.marquee.is_visible():
             self.layout.marquee.move(event)
         elif self.scrubbing:
             self._seek(event)
@@ -902,15 +911,6 @@ class Timeline(Gtk.EventBox, Zoomable, Loggable):
         self.hadj.set_value(self.hadj.get_value() + x_diff)
         y_diff = self._scroll_start_y - event.y
         self.vadj.set_value(self.vadj.get_value() + y_diff)
-
-    def _select_under_marquee(self):
-        if self.layout.marquee.props.width_request > 0:
-            clips = self.layout.marquee.find_clips()
-        else:
-            clips = []
-        self.selection.set_selection(clips, SELECT)
-
-        self.layout.marquee.hide()
 
     def update_position(self):
         for ges_layer in self.ges_timeline.get_layers():