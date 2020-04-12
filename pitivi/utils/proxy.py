ce866c52110458f5d651e71a1a4222e31cc541c3
Ritwik Puri <ritwikpuri5678@gmail.com>
2020-04-12 14:54:03 +0530

    proxy: add_job restucture
    Took some re-occuring code out of
    
    	if video_streams:..
    	else:...

--- pitivi/utils/proxy.py
+++ pitivi/utils/proxy.py
@@ -760,30 +760,23 @@ class ProxyManager(GObject.Object, Loggable):
                 if self.is_asset_queued(asset, scaling=False):
                     self.log("Asset already queued for optimization: %s", asset)
                     return
-
-            if not force_proxying:
-                if not self.__asset_needs_transcoding(asset, scaled):
-                    self.debug("Not proxying asset (proxying disabled: %s)",
-                               self.proxying_unsupported)
-                    # Make sure to notify we do not need a proxy for that asset.
-                    self.emit("proxy-ready", asset, None)
-                    return
-
-            self.__create_transcoder(asset, scaled=scaled, shadow=shadow)
         else:
+            # Scaled proxy is not for audio assets
+            scaled = False
+
             if self.is_asset_queued(asset, scaling=False):
                 self.log("Asset already queued for optimization: %s", asset)
                 return
 
-            if not force_proxying:
-                if not self.__asset_needs_transcoding(asset, scaled):
-                    self.debug("Not proxying asset (proxying disabled: %s)",
-                               self.proxying_unsupported)
-                    # Make sure to notify we do not need a proxy for that asset.
-                    self.emit("proxy-ready", asset, None)
-                    return
+        if not force_proxying:
+            if not self.__asset_needs_transcoding(asset, scaled):
+                self.debug("Not proxying asset (proxying disabled: %s)",
+                           self.proxying_unsupported)
+                # Make sure to notify we do not need a proxy for that asset.
+                self.emit("proxy-ready", asset, None)
+                return
 
-            self.__create_transcoder(asset)
+        self.__create_transcoder(asset, scaled=scaled, shadow=shadow)
 
 
 def get_proxy_target(obj):
